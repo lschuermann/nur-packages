eb13ba0c0095b1b556ece0c87b8666de1b8b2d64
Leon Schuermann <leon@is.currently.online>
2025-11-24 09:55:41 -0500

    flake.nix: add treefmt-nix integration

--- flake.nix
+++ flake.nix
@@ -1,14 +1,31 @@
 {
   description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, treefmt-nix }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+      nixpkgsForSystem = system: import nixpkgs { inherit system; };
+      treefmtBuild = pkgs: (treefmt-nix.lib.evalModule pkgs ./treefmt.nix).config.build;
     in
     {
       legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = nixpkgsForSystem system;
       });
+
       packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+
+      checks = forAllSystems (system: {
+        formatting = (treefmtBuild (nixpkgsForSystem system)).check self;
+      });
+
+      formatter = forAllSystems (system: (treefmtBuild (nixpkgsForSystem system)).wrapper);
     };
 }
