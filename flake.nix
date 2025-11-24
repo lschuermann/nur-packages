6344c1a8cc1815f84f8b9760800861ad1f22da23
Leon Schuermann <leon@is.currently.online>
2025-11-24 09:57:37 -0500

    nixfmt

--- flake.nix
+++ flake.nix
@@ -9,18 +9,23 @@
     };
   };
 
-  outputs = { self, nixpkgs, treefmt-nix }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      treefmt-nix,
+    }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
       nixpkgsForSystem = system: import nixpkgs { inherit system; };
       treefmtBuild = pkgs: (treefmt-nix.lib.evalModule pkgs ./treefmt.nix).config.build;
     in
     {
-      legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = nixpkgsForSystem system;
-      });
+      legacyPackages = forAllSystems (system: import ./default.nix { pkgs = nixpkgsForSystem system; });
 
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+      packages = forAllSystems (
+        system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system}
+      );
 
       checks = forAllSystems (system: {
         formatting = (treefmtBuild (nixpkgsForSystem system)).check self;
